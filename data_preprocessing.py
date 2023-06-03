3f9083b89e9502d25e60e975bf1a9075543b20b0
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2023-06-03 17:49:32 +0200

    finish preprocessing and add training skeleton based on lima model

---
+++ data_preprocessing.py
@@ -0,0 +1,144 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+import numpy as np
+
+from consts import *
+
+INTRO_BLURB = (
+    "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+)
+INSTRUCTION_KEY = "### Instruction:"
+RESPONSE_KEY = "### Response:"
+END_KEY = "### End"
+
+#(instruction,response) pairs will be repeated for the duration of the conversation
+PROMPT_FORMAT = """{intro}
+
+{context}
+
+{instruction_key}
+{instruction}
+
+{response_key}
+{response}
+
+{end_key}""".format(
+    intro=INTRO_BLURB,
+    context="{context}",
+    instruction_key=INSTRUCTION_KEY,
+    instruction="{instruction}",
+    response_key=RESPONSE_KEY,
+    response="{response}",
+    end_key=END_KEY,
+)
+def append_to_context_above(prev_context,role,message):
+  new_context="""
+    {new_key}
+    {new_utterance}
+    {prev_context}
+  """.format(
+      new_key="{new_key}",
+      new_utterance="{new_utterance}",
+      prev_context=prev_context)
+  if role=="assistant":
+    new_context=new_context.format(
+      new_key=RESPONSE_KEY,
+      new_utterance=message
+    )
+  else:
+    new_context=new_context.format(
+      new_key=INSTRUCTION_KEY,
+      new_utterance=message
+    )
+  return new_context
+
+def filter_top_replies(dataset):
+  to_keep=[False for _ in range(len(dataset.index))]
+  grouped_messages = dataset.groupby('parent_id')['message_id'].apply(lambda x: x.index.tolist()).reset_index()
+  dataset = dataset.merge(grouped_messages, left_on='message_id', right_on='parent_id', suffixes=('', '_grouped'),how="left")
+  dataset = dataset.rename(columns={"message_id_grouped":"all_replies"})
+  dataset['all_replies']=dataset['all_replies'].fillna("").apply(list)
+  def dfs(index):
+    to_keep[index]=True
+    curr_col=dataset.loc[index]
+    #faster version is to have a field for this processed above
+    # curr_children=dataset.loc[dataset["parent_id"]==curr_col["message_id"]]
+    curr_children=dataset.iloc[curr_col["all_replies"]]
+    curr_len=len(curr_children.index)
+    if curr_len==0:
+      return
+    top_reply_index=curr_children.index[0]
+    top_reply=curr_children.iloc[0]
+    for index,row in curr_children.iterrows():
+      if row["rank"]<top_reply["rank"]:
+        top_reply=row
+        top_reply_index=index
+    dfs(top_reply_index)
+
+  for index, row in dataset.iterrows():
+    to_keep[index]=False
+  for index, row in dataset.iterrows():
+    if row["parent_id"]==None:
+      dfs(index)
+  
+  dataset=dataset[to_keep]
+  dataset=dataset.loc[~((dataset['role'] == 'prompter') & (dataset['all_replies'].apply(len) == 0))]
+  print("data length after filtering",len(dataset.index))
+  return dataset
+
+
+def get_conversation_chains(dataset):
+  def get_parent_row(row):
+    return dataset[dataset["message_id"]==row["parent_id"]].iloc[0]
+  assistant_replies=dataset[dataset["role"]=="assistant"]
+  conversation_chains=[]
+  for index,reply_row in assistant_replies.iterrows():
+    #use only final replies to get full conversation
+    if len(reply_row["all_replies"])>0:
+      continue
+    #we split the conversation up into 3 parts:
+    #context: all previous questions and answers
+    #instruction: previous user prompt
+    #response: the expected assistant response
+    response=reply_row["text"]
+    instruction_row=get_parent_row(reply_row)
+    instruction=instruction_row["text"]
+    context=""
+    curr_row=instruction_row
+    while curr_row["parent_id"]!=None:
+      curr_row=get_parent_row(curr_row)
+      append_to_context_above(context,curr_row["role"],curr_row["text"])
+      curr_row=get_parent_row(curr_row)
+      append_to_context_above(context,curr_row["role"],curr_row["text"])
+    conversation_chain=PROMPT_FORMAT.format(
+        context=context,
+        instruction=instruction,
+        response=response,
+      )
+    conversation_chains.append(conversation_chain)
+  return conversation_chains
+
+
+def get_tokenized_dataset(conversation_chains,tokenizer):
+  conversation_dataset=[{"text":conversation} for conversation in conversation_chains]
+  tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
+  def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+  tokenized_dataset = conversation_dataset.map(tokenize_function, batched=True)
+  #TODO: convert to (x,y) samples 
+  return tokenized_dataset
+
+def preprocess_dataset(dataset):
+  dataset=filter_top_replies(dataset)
+  dataset=get_conversation_chains(dataset)
+  dataset=get_tokenized_dataset(dataset)
+  return dataset
+
+def get_preprocessed_dataset(split="train"):
+  if not split in ["train","val"]:
+    print("dataset only has 'train' and 'val' data")
+    exit(1)
+  dataset = load_dataset(DEFAULT_TRAINING_DATASET)
+  dataset_split = dataset[split].to_pandas()
+  processed_dataset=preprocess_dataset(dataset_split)
+  return processed_dataset
--- training.py
+++ training.py
@@ -1,18 +1,77 @@
-from transformers import AutoTokenizer, BloomModel
-from datasets import load_dataset
+from transformers import BloomModel
 import torch
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import LinearLR
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+import evaluate
+import os
 
+from data_preprocessing import get_preprocessed_dataset
 from consts import *
 
-# tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
-# model = BloomModel.from_pretrained(PRETRAINED_MODEL)
-dataset = load_dataset(DEFAULT_TRAINING_DATASET)
-train_dataset = dataset['train']
-val = dataset['validation'] 
-print(train_dataset[0].keys())
-#find index of "### Response: 
-for i in range(len(train_dataset)):
-  curr_inst=train_dataset[i]['labels']
-  quality_idx=curr_inst["name"].index("quality")
-  train_dataset[i]["quality"]=curr_inst["value"][quality_idx]
-  print(train_dataset[i])
+if __name__=="__main__":
+  #using same training setup as described in LIMA paper (https://arxiv.org/pdf/2305.11206.pdf)
+  num_epochs=15
+  start_learning_rate=1e-5
+  end_learning_rate=1e-6
+  beta1=0.9
+  beta2=0.95
+  weight_decay=0.1
+  batch_size=64 #using smaller model and larger batch size
+
+  torch.manual_seed(DEFAULT_SEED)
+  model = BloomModel.from_pretrained(PRETRAINED_MODEL)
+  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+  model.to(device)
+
+  train_dataset = get_preprocessed_dataset("train")
+  eval_dataset = get_preprocessed_dataset("val")
+  train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+  eval_dataloader = DataLoader(eval_dataset, batch_size=batch_size)
+  out_dir="saved_models"
+
+  num_training_steps = num_epochs * len(train_dataloader)
+  optimizer = AdamW(model.parameters(),lr=start_learning_rate,betas=(beta1,beta2),weight_decay=0.1)
+  lr_scheduler = LinearLR(optimizer, start_factor=1,end_factor=end_learning_rate/start_learning_rate, total_iters=num_training_steps)
+  progress_bar = tqdm(range(num_training_steps))
+  accuracy_metric = evaluate.load("accuracy")
+  for epoch_num in range(num_epochs):
+    model.train()
+    for batch in train_dataloader:
+      batch = {k: v.to(device) for k, v in batch.items()}
+      outputs = model(**batch)
+      loss = outputs.loss
+      loss.backward()
+
+      optimizer.step()
+      lr_scheduler.step()
+      optimizer.zero_grad()
+      progress_bar.update(1)
+
+    model.eval()
+    for batch in eval_dataloader:
+      batch = {k: v.to(device) for k, v in batch.items()}
+      with torch.no_grad():
+          outputs = model(**batch)
+
+      logits = outputs.logits
+      predictions = torch.argmax(logits, dim=-1)
+      #TODO: check how to format this
+      accuracy_metric.add_batch(predictions=predictions, references=batch["labels"])
+
+    val_accuracy=accuracy_metric.compute()
+    #checkpoint model after every 5 epochs
+
+    if epoch_num%5==0:
+      checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        # 'model_args': model_args,
+        'epoch_num': epoch_num,
+        'val_accuracy': val_accuracy,
+      }
+      print(f"saving checkpoint to {out_dir}")
+      torch.save(checkpoint, os.path.join(out_dir, f'ckpt_epoch_{epoch_num}.pt'))
+    
+  
